26a4b83ebfe684c99f7d1fc66bd9ee16c941e998
guoWork <15088138492@163.com>
2019-01-31 22:13:30 +0800

    前端页面与架构的搭建、开发完成商品类型的增删改查、商品的查询

--- manager/manager-dao/src/main/java/com/bl/dao/UserMapperEx.java
+++ manager/manager-dao/src/main/java/com/bl/dao/UserMapperEx.java
@@ -1,12 +1,5 @@
 package com.bl.dao;
 
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
-
 public interface UserMapperEx {
-
-    //根据用户标识获取用户
-    List<Integer> getOneUserByUserId(@Param(value = "userId") Integer userId);
 
 }
