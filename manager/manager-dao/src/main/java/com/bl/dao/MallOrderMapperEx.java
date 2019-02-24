303c358a2480f62902a2fcf97d91f627dcc0e966
guoWork <15088138492@163.com>
2019-02-24 12:12:21 +0800

    开发微信小程序的功能

--- manager/manager-dao/src/main/java/com/bl/dao/MallOrderMapperEx.java
+++ manager/manager-dao/src/main/java/com/bl/dao/MallOrderMapperEx.java
@@ -1,6 +1,7 @@
 package com.bl.dao;
 
 import com.bl.pojo.MallOrderVo;
+import com.bl.pojo.MallOrderWXVo;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -8,8 +9,17 @@ import java.util.Map;
 
 public interface MallOrderMapperEx {
 
+    //后台获取订单列表
     List<MallOrderVo> getMallOrderInfoList(
             @Param(value = "limitStart") Integer limitStart,
             @Param(value = "limitEnd") Integer limitEnd);
 
+    /**
+     * 微信小程序的获取订单列表
+     *      根据用户标识与订单状态
+     */
+    List<MallOrderWXVo> getWXOrderListByUserIdAndOrderStatus(
+            @Param(value = "userId") Integer userId,
+            @Param(value = "orderStatusList") String[] orderStatusArr
+    );
 }
