1373333a16da0e1ea21984cd259961431de7eb54
guoWork <15088138492@163.com>
2019-02-24 12:13:04 +0800

    用于获取微信小程序的用户信息对象

---
+++ manager/manager-pojo/src/main/java/com/bl/pojo/WxUserInfoVo.java
@@ -0,0 +1,65 @@
+package com.bl.pojo;
+
+/**
+ * 微信数据
+ */
+public class WxUserInfoVo {
+    /**
+     * 包括敏感数据在内的完整用户信息的加密数据
+     */
+    private String encryptedData;
+    /**
+     * 加密算法的初始向量
+     */
+    private String iv;
+    /**
+     * 不包括敏感信息的原始数据字符串，用于计算签名
+     */
+    private String rawData;
+    /**
+     * 使用 sha1( rawData + sessionkey ) 得到字符串，用于校验用户信息
+     */
+    private String signature;
+
+    public String getEncryptedData() {
+        return encryptedData;
+    }
+
+    public void setEncryptedData(String encryptedData) {
+        this.encryptedData = encryptedData;
+    }
+
+    public String getIv() {
+        return iv;
+    }
+
+    public void setIv(String iv) {
+        this.iv = iv;
+    }
+
+    public String getRawData() {
+        return rawData;
+    }
+
+    public void setRawData(String rawData) {
+        this.rawData = rawData;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+
+    @Override
+    public String toString() {
+        return "WxUserInfoVo{" +
+                "encryptedData='" + encryptedData + '\'' +
+                ", iv='" + iv + '\'' +
+                ", rawData='" + rawData + '\'' +
+                ", signature='" + signature + '\'' +
+                '}';
+    }
+}
