89f51ee7f65d4ba59527de3a003ec59e6ee6180b
guoWork <15088138492@163.com>
2019-02-24 12:11:24 +0800

    更改获取常量的方法

--- manager/manager-controller/src/main/java/com/bl/controller/LoginController.java
+++ manager/manager-controller/src/main/java/com/bl/controller/LoginController.java
@@ -2,7 +2,7 @@ package com.bl.controller;
 
 import com.bl.base.Response;
 import com.bl.base.LoginBean;
-import com.bl.utils.MD5;
+import com.bl.utils.securityUtils.MD5;
 import com.bl.utils.PropertiesUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.LogManager;
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Properties;
 
 @Controller
 @RequestMapping("login.do")
@@ -24,20 +23,19 @@ public class LoginController {
     @RequestMapping(params = "method=doLogin")
     @ResponseBody
     public Response doLogin(HttpServletRequest req, HttpServletResponse res) {
-        Properties properties = PropertiesUtil.readProperties("config.properties");
         String userAccount = req.getParameter("username");
         String userPassword = req.getParameter("password");
         LoginBean lb = new LoginBean();
         Response response = new Response();
         try {
-            if (StringUtils.isNotBlank(userAccount) && !properties.getProperty("userAccount").equals(userAccount)) {
+            if (StringUtils.isNotBlank(userAccount) && !PropertiesUtil.getUserAccount().equals(userAccount)) {
                 response = Response.createFailResult("账号:" + userAccount + "不存在", null);
-            }else if (StringUtils.isNotBlank(userPassword) && !(MD5.GetMD5Code(properties.getProperty("userPassword")).equalsIgnoreCase(MD5.GetMD5Code(userPassword)))) {
+            }else if (StringUtils.isNotBlank(userPassword) && !(MD5.GetMD5Code(PropertiesUtil.getUserPassword()).equalsIgnoreCase(MD5.GetMD5Code(userPassword)))) {
                 response = Response.createFailResult("账号密码错误", null);
             } else {
-                lb.setUserId(Integer.valueOf(properties.getProperty("userId")));
-                lb.setUserAccount(properties.getProperty("userAccount"));
-                lb.setUserName(properties.getProperty("userName"));
+                lb.setUserId(Integer.valueOf(PropertiesUtil.getUserId()));
+                lb.setUserAccount(PropertiesUtil.getUserAccount());
+                lb.setUserName(PropertiesUtil.getUserName());
                 req.getSession().setAttribute(LoginBean.UserLoginKey, lb);
                 response = Response.createSuccessResult("登陆成功", null);
             }
