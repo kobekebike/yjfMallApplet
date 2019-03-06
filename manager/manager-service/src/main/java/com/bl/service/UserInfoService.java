3e458ec6cc57f82e56b452a8da5c56f0d3dd226c
guoWork <15088138492@163.com>
2019-03-06 11:28:57 +0800

    新增用户信息

--- manager/manager-service/src/main/java/com/bl/service/UserInfoService.java
+++ manager/manager-service/src/main/java/com/bl/service/UserInfoService.java
@@ -3,13 +3,13 @@ package com.bl.service;
 import com.bl.base.EasyUIResult;
 import com.bl.base.Response;
 import com.bl.dao.UserInfoMapper;
-import com.bl.model.Product;
-import com.bl.model.ProductCriteria;
 import com.bl.model.UserInfo;
 import com.bl.model.UserInfoCriteria;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -31,5 +31,31 @@ public class UserInfoService {
         userInfoCriteria.setOrderByClause("create_time desc");
         List<UserInfo> list = userInfoMapper.selectByExample(userInfoCriteria);
         return new EasyUIResult(list.size(), list);
+    }
+
+    public Response saveOrUpdateUserInfo(String openId){
+        if(StringUtils.isNotBlank(openId) && !"null".equals(openId)){
+            int userInfoId = 0;
+            UserInfoCriteria userInfoCriteria = new UserInfoCriteria();
+            userInfoCriteria.createCriteria().andOpenIdEqualTo(openId);
+            List<UserInfo> list = userInfoMapper.selectByExample(userInfoCriteria);
+            if(list != null && !list.isEmpty()){//存在则修改
+                UserInfo userInfo = list.get(0);
+                userInfo.setVisitTime(new Date());
+                userInfoMapper.updateByPrimaryKeySelective(userInfo);
+                userInfoId = userInfo.getId();
+            }else{//不存在则保存
+                UserInfo user = new UserInfo();
+                user.setOpenId(openId);
+                user.setCreateTime(new Date());
+                user.setUpdateTime(new Date());
+                user.setVisitTime(new Date());
+                userInfoMapper.insertSelective(user);
+                userInfoId = user.getId();
+            }
+            return Response.createSuccessResult("访问成功", userInfoId);
+        }else{
+            return Response.createFailResult("openId为空", null);
+        }
     }
 }
