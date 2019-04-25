4341faba06a6e47585f2d5f00c5dfb8313977682
guoshengfang <15088138492@163.com>
2019-04-25 11:35:06 +0800

    增加修改方法

--- manager/manager-controller/src/main/java/com/bl/controller/UserInfoController.java
+++ manager/manager-controller/src/main/java/com/bl/controller/UserInfoController.java
@@ -1,6 +1,9 @@
 package com.bl.controller;
 
 import com.bl.base.EasyUIResult;
+import com.bl.base.Response;
+import com.bl.model.ProductWithBLOBs;
+import com.bl.model.UserInfo;
 import com.bl.service.UserInfoService;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -9,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -41,5 +45,18 @@ public class UserInfoController {
         }
         return easyUIResult;
     }
-    
+
+    /**
+     * 修改用户
+     * @param req
+     * @param res
+     * @param userInfo
+     * @return
+     */
+    @RequestMapping("updateUserInfo.do")
+    @ResponseBody
+    public Response updateUserInfo(HttpServletRequest req, HttpServletResponse res,
+                               UserInfo userInfo){
+        return userInfoService.updateUserInfo(userInfo);
+    }
 }
